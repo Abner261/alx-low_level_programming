f9cfdf9dba1e5ce58a305818f1a8e97fe1f440a7
Abner <santosabner949@gmail.com>
2023-10-09 13:14:28 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - This marks the program's starting point
+ * This program outputs the lowercase alphabet ('a' to 'z') with a newline
+ * return: 0 (success)
+ */
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+
+putchar(a);
+putchar('\n')
+Return(0);
+}
