8964f1fd809d5170abe7c8f1c13bde68ddf187f5
Abner261 <santosabner949@gmail.com>
2023-11-20 09:38:37 -0800

    A function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,35 +9,28 @@
  * I want to combine to the first one
  * Return: Null on failure.
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	char *pstr = NULL;
-	unsigned int len;
-	unsigned int x, y, z, zz;
+	unsigned int len1 = (s1 == NULL) ? 0 : 0;
+	unsigned int len2 = (s2 == NULL) ? 0 : 0;
+	char *result, *p;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	for (x = 0; s1[x] != '\0'; x++)
-		;
-	for (y = 0; s2[y] != '\0'; y++)
-		;
-	len = x + y + 1;
-	pstr = malloc(len * sizeof(char));
-
-	if (pstr == NULL)
+	result = malloc((len1 + len2 + 1) * sizeof(char));
+	if (result == NULL)
 		return (NULL);
 
-	for (z = 0; z < x; z++)
-	{
-		pstr[z] = s1[z];
-	}
-	for (zz = 0; zz < y; z++, zz++)
-	{
-		pstr[z] = s2[zz];
-	}
-	pstr[len] = s2[zz];
-	return (pstr);
+	p = result;
+	while (*s1)
+		*p++ = *s1++;
+	while (*s2)
+		*p++ = *s2++;
+
+	*p = '\0';
+	return (result);
 }
