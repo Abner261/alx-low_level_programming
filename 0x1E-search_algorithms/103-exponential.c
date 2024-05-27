ee55ab94e08b9a46b858d9e30c6ca81410ba38d4
Abner261 <santosabner949@gmail.com>
2024-05-27 06:52:17 -0700

    A function that searches for a value in a sorted array of integers using the Exponential search algorithm

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,74 @@
+#include "search_algos.h"
+
+/**
+ * _binary_search - A function that searches for a value in a
+ * sorted array of integers using the Binary search algorithm
+ *
+ * @array: A pointer to the first element of the array to search in
+ *
+ * @left: The left boundary of the subarray to search
+ *
+ * @right: The right boundary of the subarray to search
+ *
+ * @value: The value to search for
+ *
+ * Return: The index where value is located, or -1 if not found
+ */
+
+int _binary_search(int *array, size_t left, size_t right, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	while (right >= left)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
+
+	return (-1);
+}
+
+
+/**
+ * exponential_search - A function that searches for a value in a
+ * sorted array of integers using the Exponential search algorithm
+ *
+ * @array: Pointer to the first element of the array to search in
+ *
+ * @size: The number of elements in the array
+ *
+ * @value: The value to search for
+ *
+ * Return: The first index where value is located or -1 if not found
+ */
+
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+	size_t  right;
+
+	if (array == NULL || size == 0)
+		return (-1);
+	if (array[0] != value)
+	{
+		for (i = 1; i < size && array[i] <= value; i *= 2)
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	}
+
+	right = i < size ? i : size - 1;
+	printf("Value found between indexes [%ld] and [%ld]\n", i / 2, right);
+	return (_binary_search(array, i / 2, right, value));
+}
